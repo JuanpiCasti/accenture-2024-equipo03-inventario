42ac907e20971deb932679620936e4299a3ed422
Juan Pablo Castiglione <juanpablocastiglione01@gmail.com>
2024-06-24 16:26:48 -0300

    first commit

---
+++ src/main/java/ar/frba/utn/sharksales/accenture2024equipo03inventario/Accenture2024Equipo03InventarioApplication.java
@@ -0,0 +1,13 @@
+package ar.frba.utn.sharksales.accenture2024equipo03inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accenture2024Equipo03InventarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Accenture2024Equipo03InventarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/frba/utn/sharksales/accenture2024equipo03inventario/Accenture2024Equipo03InventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.frba.utn.sharksales.accenture2024equipo03inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Accenture2024Equipo03InventarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
